655be1578be5233b15e021f25c3f2c28ed553026
Sfa11eN <sfallenn@gmail.com>
2023-12-21 19:53:37 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+        System.out.println("Homework-5");
+
+        {
+            System.out.println(" Условный оператор-2 ");
+
+            System.out.println(" Задание 1 ");
+
+            int clientOS = 1;
+            if (clientOS == 0) {
+                System.out.println("Установите приложение для iOS по ссылке");
+            } else if (clientOS == 1) {
+                System.out.println("Установите приложение для Android по ссылке");
+            } else {
+                System.out.println("Некорректно введены данные");
+            }
+            System.out.println(" Задание 2 ");
+        }
+    }}
